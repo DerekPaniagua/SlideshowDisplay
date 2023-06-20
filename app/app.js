4201ad028dc331cd90ba3694bf9e992212c8f00c
DerekPaniagua <paniaguaderek@gmail.com>
2023-06-19 19:04:45 -0600

    updating google drive images in background

--- app/app.js
+++ app/app.js
@@ -3,16 +3,20 @@ const slideshow = require('./slideshow');
 const common = require('./common');
 const path = require('node:path');
 
+console.info("Starting up server...")
+
 // Setup
 let image_list = [];
 let last_index = 0;
-
 const config = common.get_config();
+check_and_update_images().then(() => {setInterval(check_and_update_images, config.google_drive_refresh_duration);});
 
+
+// Web Server
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 app.listen(3000, () => {
-    console.log("Application Listening on port 3000");
+    console.info("Application Listening on port 3000");
 });
 
 // Endpoints
@@ -26,34 +30,40 @@ app.get("/config", (req, res) => {
         calendar_update_duration: config.calendar_update_duration,
     };
     res.json(html_config);
-})
+});
 
 app.get("/image", async (req, res) => {
-    // Refresh our images everytime the slideshow restarts and our file names have changed
-    if (image_list.length === last_index){
-        let drive = slideshow.get_google_drive(config.google_credentials_path);
-        let new_image_list = await slideshow.get_file_names(drive);
-        let hasChanged = false;
-        for (let image_name of new_image_list){
-            if (!image_list.includes(image_name)){
-                hasChanged = true;
-                break;
-            }
-        }
-        if (hasChanged || new_image_list.length !== image_list.length){
-            image_list = new_image_list;
-            image_list.sort();
-            await slideshow.refresh_images(config.images_path, drive);
-        }
-        last_index = 0;
+    if (image_list.length === 0){
+        res.end()
+        return;
     }
+    console.info(`Returning ${image_list[last_index]}`);
     let full_path = path.join(__dirname, '/images', image_list[last_index]);
     res.sendFile(full_path);
-    last_index += 1;
+    last_index = (last_index + 1) % image_list.length;
 });
 
 app.get("/test", (req, res) => {
-    let drive = slideshow.get_google_drive(config.google_credentials_path);
-    slideshow.refresh_images(config.images_path, drive);
     res.end();
-})
+});
+
+// Helper Fucntions
+async function check_and_update_images(){
+    console.info("Checking google drive for image updates...");
+    let drive = slideshow.get_google_drive(config.google_credentials_path);
+    let new_image_list = await slideshow.get_file_names(drive);
+    let hasChanged = false;
+    for (let image_name of new_image_list){
+        if (!image_list.includes(image_name)){
+            hasChanged = true;
+            break;
+        }
+    }
+    if (hasChanged || new_image_list.length !== image_list.length){
+        console.info("Change detected, refreshing images...");
+        image_list = new_image_list;
+        image_list.sort();
+        await slideshow.refresh_images(config.images_path, drive);
+        console.info(`Successfully fetched ${image_list.length} images!`)
+    }
+};
--- startup.sh
+++ startup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
 git pull
-
 npm install
-
 npm run start &
-
 sleep 10
-
 firefox-esr --kiosk http://localhost:3000
