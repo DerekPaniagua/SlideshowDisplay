4f108a7cf4a899206437124d14c8f1fb1e254f68
Derek Paniagua <paniaguaderek@gmail.com>
2023-06-25 23:43:35 -0600

    pls work

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 cd ~/Repos/SlideshowDisplay
 git pull
 npm install
