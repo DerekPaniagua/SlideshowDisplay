3b5fe6b28562ac035f59200d7837c6778dd3498b
Derek Paniagua <paniaguaderek@gmail.com>
2023-06-25 22:33:21 -0600

    update for gnome

--- startup.sh
+++ startup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 git pull
 npm install
 npm run start &
-sleep 10
-firefox --kiosk http://localhost:3000
+sleep 5
+chromium-browser http://localhost:3000 --kiosk
