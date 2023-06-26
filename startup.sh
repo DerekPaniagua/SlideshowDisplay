2a147286c2257272c76f403e13d891bf0c2c2b09
Derek Paniagua <paniaguaderek@gmail.com>
2023-06-26 16:02:44 -0600

    updated startup

--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 cd ~/Repos/slideshowdisplay
-git pull
+git pull &
+sleep 5
 npm install
 npm run start:prod &
 sleep 5
