ddbf4a6dd641181c1b3606d5af3b5c3509635e30
Derek Paniagua <paniaguaderek@gmail.com>
2023-06-26 19:01:25 -0600

    updated startup for wifi connection time

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 cd ~/Repos/slideshowdisplay
+sleep 30
 git pull &
 sleep 5
 npm install
